1aad3303cb70b25fb0efbb984d80d0967f3aca35
tylernchls <tylernchls@gmail.com>
2018-05-23 13:48:55 -1000

    adds 404 page

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+// import Notfound from "./containers/NotFound";
 
 export default () =>
   <Switch>
---
+++ src/containers/NotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./NotFound.css";
+
+export default () =>
+  <div className="NotFound">
+    <h3>HMMMMM, Looks like the page you want doesn't exist</h3>
+  </div>
