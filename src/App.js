da41af89890e9337247f14cf7e32674f250e10e8
tylernchls <tylernchls@gmail.com>
2018-05-23 13:23:32 -1000

    adds linkContainer to stop page reload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import Routes from "./Routes";
 import './App.css';
@@ -16,10 +17,14 @@ class App extends Component {
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
-            <Nav pullRight>
-              <NavItem href="/signup">Signup</NavItem>
-              <NavItem href="/login">Login</NavItem>
-            </Nav>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
         </Navbar.Collapse>
         </Navbar>
         <Routes />
