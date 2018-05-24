71e71d20e7d4e20fa3417d123921f86d5560901d
tylernchls <tylernchls@gmail.com>
2018-05-23 15:10:50 -1000

    adds aws config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://ff1zbowi4m.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_hY8HMLss0",
+      APP_CLIENT_ID: "2bppjanv1iiu4rcv922t7muia6",
+      IDENTITY_POOL_ID: "us-west-2:cbf0312c-d84a-4900-99cf-ca00e4061418"
+    }
+};
