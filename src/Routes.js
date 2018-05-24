72f7ce90962625d5ac389515fb81e5a96a85f7c4
tylernchls <tylernchls@gmail.com>
2018-05-23 14:13:22 -1000

    adds login page

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 // import Notfound from "./containers/NotFound";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
   </Switch>
