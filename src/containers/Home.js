f2fb23f8b72e7b4794dcca4190396bb733881412
Tyler Nichols <tylernchls@gmail.com>
2018-07-24 16:15:11 -1000

    adds conditional render of homepage or notes page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
+import { PageHeader , ListGroup } from 'react-bootstrap';
 import "./Home.css";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      notes: []
+    };
+  }
+
+  renderNotesList(notes) {
+    return null;
+  }
+
+  renderLander() {
+    return (
+      <div className="Lander">
+        <h1>Barrel</h1>
+        <p>Your own personal cloud storage service</p>
+      </div>
+    );
+  }
+
+  renderNotes() {
+    return (
+      <div className="Notes">
+        <PageHeader>Your Files</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="Home">
-        <div className="lander">
-          <h1>Barrel</h1>
-          <p>Your own cloud storage service</p>
-        </div>
-      </div>
+        {this.props.isAuthenticated ? this.renderNotes() : this.renderLander()}
+      </div>  
     );
   }
 }
